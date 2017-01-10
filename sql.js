37c72540939658978fbd7ab45c465d70c8eadd1c
marcandy <marcandy.nj@gmail.com>
2017-01-10 14:45:57 -0700

    sql

--- sql.js
+++ sql.js
@@ -10,3 +10,9 @@ join [Table2] [abbv2] on abbv.prop=abbv2.prop where [Conditions] */
 SELECT * FROM Invoice i
 JOIN InvoiceLine il ON i.InvoiceId = il.InvoiceId
 WHERE UnitPrice > .99
+
+SELECT InvoiceDate, Total, FirstName, LastName FROM Invoice i
+JOIN Customer c ON i.CustomerId = c.CustomerId
+
+SELECT  c.FirstName, c.LastName,e.FirstName, e.LastName  FROM Customer c
+JOIN Employee e ON(e.EmployeeId = c.SupportRepId)
